c751ad4097f274d5dcac9b3e67fd1c9a6032c489
Bruno Cardoso <66095563+brsilvacardoso@users.noreply.github.com>
2021-04-22 21:36:59 -0300

    Create Funcoes-procedimentos-e-comentarios.js

---
+++ Funcoes-procedimentos-e-comentarios.js
@@ -0,0 +1,31 @@
+/* function imprimir(i){
+  console.log(i*2);
+}
+
+imprimir(5);
+imprimir(7); */
+
+
+/* function imprimir(i){
+  console.log(i);
+}
+
+function somar(a,b){
+  imprimir(a+b)
+}
+
+somar(1,1) */
+
+
+
+function imprimir(i){
+  console.log(i);
+}
+
+function somar(a,b){
+  return (a+b)
+}
+
+var resultado = somar (1,1)
+
+imprimir(resultado)
