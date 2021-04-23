25b5540a19e3ab0c187ae554109a8ce8db89c8fe
Bruno Cardoso <66095563+brsilvacardoso@users.noreply.github.com>
2021-04-22 21:20:58 -0300

    Create Operadores-ex1.js

---
+++ Operadores-ex1.js
@@ -0,0 +1,12 @@
+nomeDoFulano = "João";
+nomeDoBeltrano = "José";
+
+//let mesmoNome = (nomeDoFulano == nomeDoBeltrano);
+
+//console.log(mesmoNome);
+
+
+// adição do módulo 09
+function mesmoNome(nomeDoFulano, nomeDoBeltrano) {
+    return nomeDoFulano == nomeDoBeltrano
+}
